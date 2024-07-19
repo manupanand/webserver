479e44e17d2c8f91b017eb060fa2e0ff66cfb5ee
manupanand <manupanandindianinvestor@gmail.com>
2024-07-19 12:03:33 +0530

    new post -go rest api

--- go-server/rest-api/main.go
+++ go-server/rest-api/main.go
@@ -22,6 +22,17 @@ func getTodo(context *gin.Context){
 	context.JSON(http.StatusOK,todos)
 
 }
+func addTodo(context *gin.Context){
+	var newTodo todo//type of todo
+	//post request -get data -in JSON inrequest body -BindJSON
+	if err := context.BindJSON(&newTodo); err != nil{
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return 
+	}
+	//add todos to list
+	todos = append(todos,newTodo)
+	context.JSON(http.StatusCreated,newTodo)
+}
 // clen and server communicate use JSON-decalre inside struct
 func main(){
 	fmt.Println("test")
@@ -29,6 +40,8 @@ func main(){
 	router:=gin.Default()
 	//create enpoint-get("path",function)-function which return json data
 	router.GET("/todos",getTodo)
+	//post
+	router.POST("/todos",addTodo)
 	//run server -router.Run()
 	router.Run("localhost:9090")
 	// fmt.Println("test")
