3d8a10e946206a491516131e9993bbf066f45ed2
manupanand <manupanandindianinvestor@gmail.com>
2024-12-01 00:07:47 +0530

    new sql

--- node-sql/src/index.ts
+++ node-sql/src/index.ts
@@ -11,18 +11,29 @@ try{
         if(pgClient){
             
                 try{
-                    const result=await pgClient.query(`CREATE TABLE users( id SERIAL PRIMARY KEY,
+                    const result=await pgClient.query(`CREATE TABLE IF NOT EXISTS users( id SERIAL PRIMARY KEY,
                         username VARCHAR(50) UNIQUE NOT NULL,
                         email VARCHAR(255) UNIQUE NOT NULL,
                         password VARCHAR(255) NOT NULL,
                         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);`)
-                        if(result){
-                            console.log(`user created`,result)
-                        }else(
-                            console.log(`error in creating database`)
-                        )
+                      
+                         // Insert default users
+                            const insertUsersQuery = `
+                            INSERT INTO users (username, email, password) 
+                            VALUES
+                                ('user1', 'user1@example.com', 'password1'),
+                                ('user2', 'user2@example.com', 'password2'),
+                                ('user3', 'user3@example.com', 'password3')
+                            ON CONFLICT (username) DO NOTHING;
+                        `;
+                        const insert = await pgClient.query(insertUsersQuery);
+                        console.log("Default users added successfully.");
                 }catch(error){
                     console.log(`user creation error`,error)
+                }finally {
+                    // Disconnect from the database
+                    await pgClient.end();
+                    console.log("Disconnected from the database.");
                 }
         }
     }
