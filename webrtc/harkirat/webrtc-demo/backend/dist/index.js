acff6fce1d0dedef0be673230db598f4716d4c6b
manupanand <manupanandindianinvestor@gmail.com>
2024-12-06 21:56:02 +0530

    new code

--- webrtc/harkirat/webrtc-demo/backend/dist/index.js
+++ webrtc/harkirat/webrtc-demo/backend/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("test");
+Object.defineProperty(exports, "__esModule", { value: true });
+//web socket server
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+wss.on("connection", (socket) => {
+    socket.on("message", (data) => {
+        console.log(data.toString());
+    });
+});
--- webrtc/harkirat/webrtc-demo/backend/src/index.ts
+++ webrtc/harkirat/webrtc-demo/backend/src/index.ts
@@ -5,5 +5,14 @@ import { WebSocketServer,WebSocket } from "ws";
 const wss= new WebSocketServer({port:8080})
 
 wss.on("connection",(socket)=>{
-    
+    socket.on("message",(data)=>{
+    console.log(data.toString());
+    const parsedData=JSON.parse(data as unknown as string)
+    if(parsedData.type =="join"){
+
+    }
+    if(parsedData.type =="chat"){
+
+    }
+  })    
 })
