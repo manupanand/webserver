20f9f2c43259b52615c2a2c44d743e1c7aa76065
manupanand <manupanandindianinvestor@gmail.com>
2024-12-25 00:23:48 +0530

    new async pg

---
+++ python/api-fastapi-psycopg2/app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from typing  import Optional
+import os
+#psycopg not working
+import asyncpg
+
+
+
+load_dotenv()
+#db config
+db_url=os.getenv("DB_URL")
+print(os.getenv("DB_URL"),db_url)
+conn_pool:Optional[asyncpg.Pool]=None
+
+# async def init_postgres()-> None:
+#     global conn_pool
+#     try:
+#         conn_pool= await asyncpg.create_pool()
+
+# async def close_postgres()-> None:
+
+# @asynccontextmanager
+# async def lifespan(app:FastAPI):
+#     await #init postgres
+#     yield
+#     await #close postgres
+
+
+
+# app:FastAPI= FastAPI(lifespan=lifespan)
+
+# @app.get('/')
+# def get_main():
+#     return {"message":"got main route"}
