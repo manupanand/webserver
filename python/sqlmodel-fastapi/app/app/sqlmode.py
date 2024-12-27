68f6229e1bdedd57fc2bb7e8cdfcef3ab520d6a6
manupanand <manupanandindianinvestor@gmail.com>
2024-12-28 00:21:40 +0530

    new sqlalchemy -sqlmodel

---
+++ python/sqlmodel-fastapi/app/app/sqlmode.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from sqlmodel import Field,SQLModel,Session,create_engine,select
+from dotenv import load_dotenv
+import os
+load_dotenv()
+db_url=os.getenv("DB_URL")
+
+class User(SQLModel,table=True):
+    id:Optional[int]=Field(default=None,primary_key=True)
+    name: str
+    secret_name: str
+    age:Optional[int]=None
+# create rows of model as instance of model
+user_1=User(name="manu p",secret_name="mpa",age=26)
+user_2=User(name="neeraj",secret_name="pappan",age=26)
+
+
+#create engine
+engine=create_engine("sqlite3:///database.db")
+
+SQLModel.metadata.create_all(engine)
+
+with Session(engine) as session:
+    session.add(user_1)
+    session.add(user_2)
+    session.commit()
